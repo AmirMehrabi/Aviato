06da4de150f5ee7d243e75ce384b2a608ef6f14c
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2026-05-30 22:40:19 +0330

    Writing tests for delete function

--- tests/Unit/ProxmoxServiceDeleteTest.php
+++ tests/Unit/ProxmoxServiceDeleteTest.php
@@ -10,10 +10,10 @@ use Tests\TestCase;
 
 class ProxmoxServiceDeleteTest extends TestCase
 {
-    public function test_delete_vm_sends_purge_and_unreferenced_disk_cleanup_payload(): void
+    public function test_delete_vm_sends_purge_and_unreferenced_disk_cleanup_parameters_without_body(): void
     {
         Http::fake([
-            'https://pve.local:8006/api2/json/nodes/pve1/qemu/101' => Http::response(['data' => 'UPID:delete'], 200),
+            'https://pve.local:8006/api2/json/nodes/pve1/qemu/101*' => Http::response(['data' => 'UPID:delete'], 200),
         ]);
 
         $result = app(ProxmoxService::class)->deleteVm($this->server(), 'pve1', 101);
@@ -25,10 +25,35 @@ class ProxmoxServiceDeleteTest extends TestCase
         ], $result['payload']);
 
         Http::assertSent(function (Request $request): bool {
+            parse_str((string) parse_url($request->url(), PHP_URL_QUERY), $query);
+
             return $request->method() === 'DELETE'
-                && $request->url() === 'https://pve.local:8006/api2/json/nodes/pve1/qemu/101'
-                && (int) $request->data()['purge'] === 1
-                && (int) $request->data()['destroy-unreferenced-disks'] === 1;
+                && str_starts_with($request->url(), 'https://pve.local:8006/api2/json/nodes/pve1/qemu/101?')
+                && $request->data() === []
+                && (int) $query['purge'] === 1
+                && (int) $query['destroy-unreferenced-disks'] === 1;
+        });
+    }
+
+    public function test_shutdown_vm_uses_shutdown_endpoint_with_timeout_and_force_stop(): void
+    {
+        Http::fake([
+            'https://pve.local:8006/api2/json/nodes/pve1/qemu/101/status/shutdown' => Http::response(['data' => 'UPID:shutdown'], 200),
+        ]);
+
+        $result = app(ProxmoxService::class)->shutdownVm($this->server(), 'pve1', 101, false);
+
+        $this->assertSame('UPID:shutdown', $result['task_id']);
+        $this->assertSame([
+            'timeout' => 60,
+            'forceStop' => 1,
+        ], $result['payload']);
+
+        Http::assertSent(function (Request $request): bool {
+            return $request->method() === 'POST'
+                && $request->url() === 'https://pve.local:8006/api2/json/nodes/pve1/qemu/101/status/shutdown'
+                && (int) $request->data()['timeout'] === 60
+                && (int) $request->data()['forceStop'] === 1;
         });
     }
 
