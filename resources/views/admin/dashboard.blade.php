6831df344128de94cea8c3983b4ffa031d5c64ce
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2026-08-08 13:24:38 +0330

    Fixed a minor issue

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -26,6 +26,7 @@
         </header>
         
 
+
         <section class="mt-4 grid grid-cols-2 gap-2.5 sm:grid-cols-3 xl:grid-cols-5" aria-label="خلاصه وضعیت">
             @foreach ($statusStrip as $item)
                 @php
