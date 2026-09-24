63b65fa0c06c0c0f02c014f5b2a58d52c24d9128
Amir Mehrabian <aut0run2011@gmail.com>
2026-09-24 18:52:28 +0330

    Fixed a minor issue

--- deploy.php
+++ deploy.php
@@ -38,7 +38,7 @@ after('deploy:shared', 'deploy:prepare_storage');
 // Used by GitHub Actions or your local computer.
 // This connects to the production server by SSH.
 host('production')
-    ->set('hostname', '5.202.19.75')
+    ->set('hostname', '5.202.19.100')
     ->set('remote_user', 'deploy')
     ->set('deploy_path', '/var/www/html/aviato')
     ->set('branch', 'master');
